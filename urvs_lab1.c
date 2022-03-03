8f958c6ae3b2cffc6ea45055f347405826ef9914
Vov4ik3587 <galstyan.vova@gmail.com>
2022-03-04 06:14:12 +0700

    Добавлены комментарии

--- urvs_lab1.c
+++ urvs_lab1.c
@@ -4,7 +4,7 @@
 #include <dirent.h>
 #include <string.h>
 
-char *makePwd(char *str, char (*pwd)[100], int col);
+char *makePwd(char *str, char (*pwd)[100], int amt);
 void checkDir(char *argv);
 
 int main(int argc, char *argv[])
@@ -13,65 +13,89 @@ int main(int argc, char *argv[])
         printf("Invalid input data: enter the name of only one file");
     else
         checkDir(argv[1]);
-    
+
     return 0;
 }
-
+//----------------------------------------------------------------------
+/** 
+ * Функция, которая определяет имена тех каталогов в каталоге,
+ * в которых есть каталоги
+ * @param argv имя целевого каталога, в котором будет происходить поиск 
+**/
 void checkDir(char *argv)
 {
     char pwd[3][100];
     char str[100];
     strcpy(pwd[0], argv);
-    DIR *dir;
-    struct dirent *ent;
-    if ((dir = opendir(argv)))
+    DIR *dir;             // внутренняя структура для работы с типом dirent
+    struct dirent *entry; //структура записей, содержащих информацию о каталоге
+
+    // открываем поток целевого каталога
+    if (dir = opendir(argv)) 
     {
-        while ((ent = readdir(dir)))
-        {
-            struct stat st1;
-            stat(ent->d_name, &st1);
-            strcpy(pwd[1], ent->d_name);
-            if (S_ISDIR(st1.st_mode) && (ent->d_name)[0] != '.')
-            {
+        // считываем файлы в каталоге по очереди
+        while (entry = readdir(dir)) 
+        { 
+            struct stat st1; // структура для сохранения информации
+            stat(entry->d_name, &st1);
+            strcpy(pwd[1], entry->d_name); // Копируем имя каталога в массив pwd
+
+            // Проверяем, является ли файл каталогом и не каталогом "."
+            if (S_ISDIR(st1.st_mode) && (entry->d_name)[0] != '.')
+            { 
+                // Если файл является каталогом, открываем для него поток
+                // и начинаем считывать файлы в нем
                 DIR *dir1;
                 struct dirent *ent1;
-                if ((dir1 = opendir(makePwd(str, pwd, 2))))
+
+                if (dir1 = opendir(makePwd(str, pwd, 2)))
                 {
                     struct stat st2;
+
                     while ((ent1 = readdir(dir1)) != NULL)
                     {
                         strcpy(pwd[2], ent1->d_name);
                         stat(makePwd(str, pwd, 3), &st2);
+
                         if (S_ISDIR(st2.st_mode) && (ent1->d_name)[0] != '.')
                         {
-                            printf("%s\n", ent->d_name);
+                            // Если файл является каталогом, то
+                            // выводим его имя в консоль
+                            printf("%s\n", entry->d_name);
                             break;
                         }
                     }
+
                     closedir(dir1);
                 }
                 else
-                {
-                    printf("%s - no such directory or not enough rights\n", ent->d_name);
-                }
+                    printf("%s - no such directory or not enough rights\n", entry->d_name);
             }
         }
+
         closedir(dir);
     }
     else
-    {
         printf("%s - no such directory or not enough rights\n", argv);
-    }
 }
-
-char *makePwd(char *str, char (*pwd)[100], int col)
+//------------------------------------------------------------------------------------------
+/**
+ * Составляет относительный путь дочерних каталогов по отношению к целевому
+ * 
+ * @param str строка, в которой будет собираться путь
+ * @param pwd составная часть итогового пути
+ * @param amt количество частей
+ * @return Строка, содержащая путь
+**/
+char *makePwd(char *str, char (*pwd)[100], int amt)
 {
     strcpy(str, ".");
-    for (int i = 0; i < col; i++)
+
+    for (int i = 0; i < amt; i++)
     {
         strcat(str, "/");
         strcat(str, pwd[i]);
     }
-    
+
     return str;
 }
